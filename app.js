1e8d16937baead5e8397add0f965dfaf1e7bdfb9
Aleksandra Siabriuk <alexsbrk91@gmail.com>
2018-05-08 22:54:17 -0400

    basic structure

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const ejs = require('ejs')
+const multer = require('multer')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const cookieParser = require('cookie-parser')
+const {Client} = require('pg')
+
+const PORT = process.env.PORT || 3000
+
+
+const app = express()
