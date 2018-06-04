e89d1f1b745a8e6c638526dbdc55a400d13988d5
Joseph Foti <eljofo@gmail.com>
2018-06-03 22:03:46 -0400

    Clean up and Seach Implementation (Users only)

--- app-prod.js
+++ app-prod.js
@@ -46,6 +46,7 @@ const sequelize = new Sequelize('barkspace', 'postgres', 'Giraffes94', {
 		$and: Op.and,
 		$or: Op.or,
 		$eq: Op.eq,
+		$regexp: Op.regexp,
 		$like: Op.like,
 		$iLike: Op.iLike
 	}
@@ -87,7 +88,7 @@ sequelize.sync()
 
 
 
-// ____________________________________CREATE A RECORD
+// ----------------------------------------------------------------------------- CREATE A RECORD
 
 // User.create({
 //
@@ -100,27 +101,11 @@ sequelize.sync()
 //
 // });
 
-// Post.create
 
 
 
 
-
-
-// const sessionStore = new SequelizeStore({
-//     db: sequelize
-//   });
-
-
-// sessionStore.sync();
-
-// ------------------------------------ Error messages
-// NOTE should be moved to separate file
-
-var errMsg = '';
-
-
-// ------------------------------------- Passport Init
+// ----------------------------------------------------------------------------- PASSPORT JS INIT
 
 app.use(cookieParser());
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
@@ -128,12 +113,11 @@ app.use(require('express-session')({ secret: 'keyboard cat', resave: false, save
 passport.use(new Strategy(
 
 	(username, password, cb)=>{
-
+		// NOTE User / Password confirmation for passportJS login
 		// use squelize search for first data entry with username feild match
 		User.findOne({
 			where: {
 				username: {
-					// case unsensitive username
 					$iLike : `${username}`
 				}
 			}
@@ -169,53 +153,93 @@ passport.deserializeUser(function(id,cb){
 
 });
 
-// tempLogin declaired globally to substitute for passportJS
-var tempLogin = '';
+
+
+// ----------------------------------------------------------------------------- MULTER
 
 
 
+// STORAGE OBJECT DEFINITION
 
-
-
-
-
-
-
-
-
-
-// ------------------------------------------------------- Login
-
-app.get('/signup',(req,res)=>{
-
-  res.render('login',{errMsg});
-
+const storage = multer.diskStorage({
+destination: './public/images/posts',
+filename: (req, file, cb)=>{
+   cb(null, Date.now() + (file.originalname) );
+}
 })
 
 
-// ------------------------------------------------------- Login post
+// UPLOAD PROCESS DEFINITION
 
-app.post('/login',
-
-	passport.authenticate('local', {failureRedirect: '/signup'}),
-
-	(req,res)=>{
-		res.redirect('/profile');
-
-});
+const upload = multer({storage: storage}).single('postpic')
 
 
 
-// ------------------------------------------------------- Register post
+
+// ----------------------------------------------------------------------------- GLOBAL VARS
+
+
+var tempLogin = '';
+// tempLogin declaired globally to substitute for passportJS
+// NOTE depreciated, used currently to test for whether or not login / logout
+// is displayed on _nav partial. Needs update
+
+var errMsg = '';
+// Error messages placeholder var
+// NOTE should be moved to separate file
+
+
+
+
+
+// ----------------------------------------------------------------------------- ROUTES
+
+
+app.get('/', (req, res)=>{
+
+		Post.findAll().then(rows=>{
+
+		if (req.user) {
+			console.log('user logged in');
+			tempLogin = req.user.dataValues.fname
+			return res.render('home', {tempLogin: tempLogin, rows: rows});
+		}
+		tempLogin = '';
+		return res.render('home', {tempLogin: tempLogin, rows: rows});
+
+		});
+
+	});
+
+
+// ----------------------------------------------------------------------------- SIGN IN | SIGN UP PORTAL
+
+app.get('/signup',(req,res)=>{
+
+	  res.render('login',{errMsg});
+
+	});
+
+
+// ----------------------------------------------------------------------------- LOGIN POST
+
+
+app.post('/login', passport.authenticate('local', {failureRedirect: '/signup'}), (req,res)=>{
+
+			res.redirect('/profile');
+
+	});
+
+
+// ----------------------------------------------------------------------------- REGISTER POST
 
 app.post('/register',(req,res)=>{
 
-	// shortens bodyParser call
 	let data = req.body;
 
 	// check for errors, if error render with error message and return before user creation
 	// else create user in table user3s
-	// NOTE passportJS and bycrypt go here
+	// NOTE bycrypt goes here
 
 	if (data.password !== data.password2) {
 		errMsg = 'Password Data does not match, try again'
@@ -225,22 +249,23 @@ app.post('/register',(req,res)=>{
 		return res.render('login',{errMsg});
 	}
 
-	// check to see if username already exists in database
+	// check to see if username already exists in database. If it does, render
+	// error msg before allowing data creation
 	User.findOne({
 		where: {
 			username: {
 				$like: `${data.username}`
 			}
 		}
-	}).then(data=>{
+	}).then(userNameAlreadyAssigned=>{
 		console.log(data);
-		if (data) {
+		if (userNameAlreadyAssigned) {
 			console.log('username exists');
 			errMsg = 'Username already in use. Please select a new username';
 			usernameSafe = false;
 			return res.render('login',{errMsg});
 		} else {
-			console.log('username not safe');
+			console.log('username safe');
 			User.create({
 					username: data.username,
 					fname: data.fname,
@@ -255,21 +280,15 @@ app.post('/register',(req,res)=>{
 		}
 	});
 
-
-
-
-
-  // client.query(``)
-
 });
 
-
+// ----------------------------------------------------------------------------- GET USER PROFILE
 
 app.get('/profile',	require('connect-ensure-login').ensureLoggedIn('/signup'), (req,res)=>{
 
 	var data = req.user.dataValues;
 
-
+	// Find all posts by user, render into feeder
 	Post.findAll({
 		where: {
 			username: {
@@ -285,67 +304,37 @@ app.get('/profile',	require('connect-ensure-login').ensureLoggedIn('/signup'), (
 
 
 });
-  // multer
-
-
-app.get('/logout',(req,res)=>{
-	req.logout();
-	res.redirect('/');
-});
-  // Tag search
-
-
-	// ______________________________________MULTER ___________________//
-
-
-	// ______________________________________STORAGE OBJECT DEFINITION
-
-	const storage = multer.diskStorage({
-	destination: './public/images/posts',
-	filename: (req, file, cb)=>{
-	   cb(null, Date.now() + (file.originalname) );
-	}
-	})
-
-	// ______________________________________UPLOAD PROCESS DEFINITION
-
-
-	const upload = multer({storage: storage}).single('postpic')
 
 
 
-	// ______________________________________ UPLOAD
 
+// ----------------------------------------------------------------------------- UPLOAD POST PICTURE | MULTER
 
 app.post('/post-picture',require('connect-ensure-login').ensureLoggedIn('/signup'), (req,res)=>{
 
-
-
 	upload(req, res, (err)=>{
-		// if (!req.file) {
-		// 	console.log('no pic!');
-		// 	return res.send('<h1>Nope!</h1>')
-		// }
+
 		if(err){
 		console.log(err)
 		}
-	// console.log(req.body)
-	// console.log(req.file)
-
 
 	  Post.create({
 	  // sequelize
-	postpic: req.file.filename
-	})
-	.then((x)=>{
-		// NOTE: something good! NOTE
-	var filename = './images/posts/' + req.file.filename
-	var id = x.dataValues.id
-	return res.render('editPost',{filename:filename,id:id});
-	})
-	  });
+			postpic: req.file.filename
+		})
+		.then((x)=>{
+				// NOTE: something good! NOTE
+			var filename = './images/posts/' + req.file.filename
+			var id = x.dataValues.id
+			return res.render('editPost',{filename:filename,id:id});
+		});
+
+	});
 
 });
+
+
+// ----------------------------------------------------------------------------- UPLOAD POST DETAILS | MULTER
 
 app.post('/post-details', require('connect-ensure-login').ensureLoggedIn('/signup'), (req, res)=>{
 
@@ -354,8 +343,8 @@ app.post('/post-details', require('connect-ensure-login').ensureLoggedIn('/signu
 	let date = new Date().getTime();
 	let data = req.user.dataValues;
 
-	// this needs to be altered
-
+	// two form system because image needs to be accepted through a specific data
+	// type in form. post-picture creates new post
 	Post.find({ where: { id: req.body.id } })
 	  .then(function (post) {
 	    // Check if record exists in db
@@ -374,34 +363,54 @@ app.post('/post-details', require('connect-ensure-login').ensureLoggedIn('/signu
 	    } else {
 				console.log('mistake!');
 			}
-	  });
-
-});
-
-
-
-
-
-app.get('/', (req, res)=>{
-
-	Post.findAll().then(rows=>{
-
-		if (req.user) {
-			console.log('user logged in');
-			tempLogin = req.user.dataValues.fname
-			return res.render('home', {tempLogin: tempLogin, rows: rows});
-		}
-	 tempLogin = '';
-	 return res.render('home', {tempLogin: tempLogin, rows: rows});
+	 });
 
 	});
 
+// ----------------------------------------------------------------------------- SEARCH REQUESTS
+
+app.post('/searchbar', (req,res)=>{
+
+	var searchQ = req.body.search;
+	var catagory = req.body.catagory;
+	var reg = searchQ + '.*';
+
+	if ( catagory === 'users' ) {
+		Post.findAll({
+			where: {
+				 username: {
+					  $regexp: reg
+				 }
+			}
+		}).then(rows=>{
+			console.log(rows);
+			if (rows) {
+				return res.render( 'search', {rows:rows,tempLogin:tempLogin} )
+			} else {
+				return res.render( 'search', {rows:false,tempLogin:tempLogin} )
+			}
+		})
+	} else {
+		Post.findOne({
+			where: {
+				 tags: {
+						$regexp: reg
+				 }
+			}
+		}).then(rows=>{
+			res.render( 'search', {rows:rows,tempLogin:tempLogin} )
+		})
+	}
+
+})
+
+// ~* '^[abc]{3}$';
+
+
+app.get('/logout',(req,res)=>{
+	req.logout();
+	res.redirect('/');
 });
-
-
-
-
-
 
 
 app.listen(PORT, ()=>{
